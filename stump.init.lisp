0fcb22a52e52152cd563ba2f462202dfa6e32d84
Ahmed Shariff <ahmed.shariff@live.com>
2018-07-03 20:57:46 +0530

    Update stump.init.lisp

--- stump.init.lisp
+++ stump.init.lisp
@@ -10,12 +10,28 @@
 (run-shell-command "feh --randomize --bg-fill /media/Files/Pictures/Background/*")
 (run-shell-command "xcompmgr -c &")
 (run-shell-command "conky -c .conky/custom/conky.conf &")
+(run-shell-command "xbingkeys")
 
-
-(define-key *root-map* (kbd "c") "exec xfce4-terminal")
+(define-key *root-map* (kbd "c") "exec xfce4-terminal --hide-menubar")
 (define-key *root-map* (kbd "e") "exec (unset SBCL_HOME;emacs &)")
 (define-key *root-map* (kbd "w") "frame-windowlist")
 (define-key *root-map* (kbd ".") "mymenu")
+(define-key *root-map* (kbd "C-o") "gother")
+(define-key *root-map* (kbd "C-TAB") "call-function-and-frame-windowlist (next)")
+(define-key *root-map* (kbd "C-M-TAB") "call-function-and-frame-windowlist (prev)")
+
+(defcommand call-function-and-frame-windowlist (&optional body) (:rest)
+  (when body
+    (eval-line body))
+  (let* ((group (current-group))
+	 (frame (tile-group-current-frame group)))
+    (echo-windows *window-format* (current-group) (frame-windows group frame))))
+
+(defcommand reboot () ()
+  (run-shell-command "reboot"))
+(defcommand shutdown () ()
+  (run-shell-command "shutdown -h now"))
+
 
 (run-commands "mode-line")
 (setf *message-window-padding* 150)
@@ -33,25 +49,7 @@
 
 (defvar *power-status* "upower -i $(upower -e | grep BAT) |grep -E \"state|to\ empty|to\ full|percentage\"")
 
-
-(defcommand mymenu () ()
-  (labels ((pick (options)
-	     (let ((selection (stumpwm::select-from-menu (current-screen) options "")))
-	       (cond
-		 ((null selection)
-		  (throw 'stumpwm::error "Abort."))
-		 ((stringp (second selection))
-		  (second selection))
-		 ((consp (cadr selection))
-		  (pick (cdr selection)))
-		 (t
-		  (cddr selection))))))
-    (let ((choice (pick *app-menu*)))
-      (if (consp choice)
-	  (run-shell-command (caar choice) (cdar choice));(run-shell-command (car a))
-	  (run-shell-command choice)))))
-
-(defparameter *app-menu* '(("Opera" "opera")
+(defparameter *app-menu* `(("Opera" "opera")
 			   ("Media"
 					;sub-menu
 			    ("Music" "vlc /media/Files/Music")
@@ -59,12 +57,45 @@
 			    ("VLC player" "vlc"))
 			   ("File Manager" "thunar")
 			   ("Check power status" t (*power-status* t)) ;; "upower -i $(upower -e | grep BAT) |grep -E \"state|to\ empty|to\ full|percentage\""
-			   ("Change background" "feh --randomize --bg-fill /media/Files/Pictures/Background/*")
-			   ("Blank screen" "xset dpms force off;i3lock -f -r 4 -s 2")))
+			   ("Screen settings"
+			    ("Change background" "feh --randomize --bg-fill /media/Files/Pictures/Background/*")
+			    ("Blank screen" "xset dpms force off;i3lock -f -r 4 -s 2")
+			    ("Brightness -" ,(lambda ()
+					       (concatenate 'string
+							    "light "
+							    (if (< (read-from-string (run-shell-command "light -G" t)) 20)
+								"-S 20"
+								"-U 12"))))
+			    ("Brightness +" "light -A 12")
+			    ("Brightness set" ,(lambda ()
+						 (concatenate 'string
+							      "light -S "
+							      (read-one-line (current-screen) "Brightness Value: ")))))))
+
+(defcommand mymenu () ()
+  (labels ((pick (options)
+	     (let ((selection (stumpwm::select-from-menu (current-screen) options "")))
+;	       (echo (funcall (second selection))))))
+	       (cond
+		 ((null selection)
+		  (throw 'stumpwm::error "Abort."))
+		 ((equalp (type-of (second selection)) 'function)
+		  (funcall (second selection)))
+		 ((consp (cadr selection))
+		  (pick (cdr selection)))
+		 ((stringp (second selection))
+		  (second selection))
+		 (t
+		  (cddr selection))))))
+    (let ((choice (pick *app-menu*)))
+      (if (consp choice)
+	  (run-shell-command (caar choice) (cdar choice));(run-shell-command (car a))
+	  (run-shell-command choice)))))
 							
-(defun shell-command-fn (command) "Run a shell command and display output to screen.
-    This must be used in a functional side-effects-free style! If a program does not
-    exit of its own accord, Stumpwm might hang!"
+(defun shell-command-fn (command) "Run a shell command and display
+    output to screen.  This must be used in a functional
+    side-effects-free style! If a program does not exit of its own
+    accord, Stumpwm might hang!"
   (let ((s (run-shell-command command t)))))    
 
 (defcommand shell-command (command)
@@ -166,3 +197,13 @@
       (list "%d     [ %n ]     Net: %I | Power: "
       ;(list "%d     [^[^(:bg \"#555511\") %n ^]]     Net: %I | Power: "
 	    `(:eval (battery-status))))
+
+(defcommand gnew-train () ()
+  (add-group (current-screen) "train")
+  (restore-from-file "~/.stumpwm.d/settings/train-layout")
+  (restore-window-placement-rules "~/.stumpwm.d/settings/train-window-rules")
+  (run-shell-command "xfce4-terminal --hide-menubar --initial-title 1 -x htop")
+  (run-shell-command "xfce4-terminal --hide-menubar --initial-title 2 -x watch -n 1 nvidia-smi")
+  (run-shell-command "xfce4-terminal --hide-menubar --initial-title 4 -x journalctl -f ")
+  (run-shell-command "xfce4-terminal --hide-menubar --initial-title 3 -x watch -n 1 sensors")
+  (run-shell-command "xfce4-terminal --hide-menubar --initial-title 0"))
