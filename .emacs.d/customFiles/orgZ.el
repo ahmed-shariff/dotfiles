b828c389a481e8c9af8f27386ef2d3ed0bb157e8
Ahmed Shariff <shariff.mfa@outlook.com>
2021-11-25 19:24:31 -0600

    Adding org-roam

--- .emacs.d/customFiles/orgZ.el
+++ .emacs.d/customFiles/orgZ.el
@@ -433,6 +433,23 @@
 
 (use-package bibtex-completion)
 
+;; (use-package org-roam
+;;   :ensure t
+;;   :custom
+;;   (org-roam-directory (file-truename org-brain-path))
+;;   :bind (("C-c n l" . org-roam-buffer-toggle)
+;;          ("C-c n f" . org-roam-node-find)
+;;          ("C-c n g" . org-roam-graph)
+;;          ("C-c n i" . org-roam-node-insert)
+;;          ("C-c n c" . org-roam-capture)
+;;          ;; Dailies
+;;          ("C-c n j" . org-roam-dailies-capture-today))
+;;   :config
+;;   (org-roam-db-autosync-mode)
+;;   ;; If using org-roam-protocol
+;;   ;; (require 'org-roam-protocol)
+;;   )
+
 (use-package ivy-bibtex
   :after (org-ref)
   :config
