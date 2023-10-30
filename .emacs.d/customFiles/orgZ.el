b90b5bcc42af87ed175fde6297588227cd98d566
Shariff Faleel <shariff.mfa@outlook.com>
2023-10-30 11:05:36 -0700

    [o] clenup

--- .emacs.d/customFiles/orgZ.el
+++ .emacs.d/customFiles/orgZ.el
@@ -1,24 +1,5 @@
 ;;; orgZ.el --- a simple package                     -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2014  Nic Ferrier
-
-;; Author: Nic Ferrier <nferrier@ferrier.me.uk>
-;; Keywords: lisp
-;; Version: 0.0.1
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 ;;; Commentary:
 ;;my custom org setup
 ;;; Code:
@@ -1491,9 +1472,7 @@ If prefix arg used, search whole db."
 
   (defun okm-org-roam-preview-kills-yank ()
     (interactive)
-    (em okm-org-roam-preview-kills)
     (dolist (content okm-org-roam-preview-kills)
-      (em content)
       (insert content "\n\n"))
     (setq okm-org-roam-preview-kills nil))
 
