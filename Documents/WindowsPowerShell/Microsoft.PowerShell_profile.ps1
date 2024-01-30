63d3d932cdabd60990830367e232b18e8baf4edd
Ahmed Shariff <shariff.mfa@outlook.com>
2024-01-30 00:59:28 -0800

    [w] Adding powershell vi bindings

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,23 @@
 Invoke-Expression (oh-my-posh init pwsh --config "$HOME\.config\oh-my-posh\config.json")
 
+# rebind keys
+Set-PSReadlineOption -EditMode vi
+Set-PSReadLineOption -ViModeIndicator Cursor
+Set-PSReadLineOption -BellStyle Visual
+Set-PSReadLineKeyHandler -Chord 'Alt+q' -Function ViCommandMode
+Set-PSReadLineKeyHandler -Chord 'Ctrl+a' -Function BeginningOfLine -ViMode Insert
+Set-PSReadLineKeyHandler -Chord 'Ctrl+e' -Function MoveToEndOfLine -ViMode Insert
+Set-PSReadLineKeyHandler -Chord 'Ctrl+a' -Function BeginningOfLine -ViMode Command
+Set-PSReadLineKeyHandler -Chord 'Ctrl+e' -Function MoveToEndOfLine -ViMode Command
+Set-PSReadLineKeyHandler -Chord 'Ctrl+f' -Function ForwardChar
+Set-PSReadLineKeyHandler -Chord 'Ctrl+b' -Function BackwardChar
+Set-PSReadLineKeyHandler -Chord 'Alt+f' -Function ForwardWord
+Set-PSReadLineKeyHandler -Chord 'Alt+b' -Function BackwardWord
+Set-PSReadLineKeyHandler -Chord 'Alt+n' -Function NextHistory
+Set-PSReadLineKeyHandler -Chord 'Alt+p' -Function PreviousHistory
+Set-PSReadLineKeyHandler -Chord 'Alt+F' -Function SelectForwardWord
+Set-PSReadLineKeyHandler -Chord 'Alt+B' -Function SelectBackwardWord
+
+Set-PSReadLineOption -PredictionViewStyle ListView
+Import-Module -Name Terminal-Icons
+
