6ea2a8c5c191ab3cf06e5ec42c35455f9d307b3f
Shariff Faleel <shariff.mfa@outlook.com>
2026-06-30 12:23:09 -0700

    [w] Add PowerShell readline and fzf setup
    Import PSReadLine, PSFzf, and Terminal-Icons in the PowerShell profile
    to enable enhanced shell behavior and file icons. Configure PSReadLine
    key bindings for word and line navigation, enable history predictions,
    and wire up PSFzf shortcuts plus tab completion to improve interactive
    command-line productivity.

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,8 @@
+# Import the modules
+Import-Module PSReadLine
+Import-Module PSFzf
+Import-Module -Name Terminal-Icons
+
 Invoke-Expression (oh-my-posh init pwsh --config "$HOME\.config\oh-my-posh\config.json")
 
 function config-git {
@@ -5,3 +10,27 @@ function config-git {
 }
 
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+# Set-PSReadLineKeyHandler -Chord 'Ctrl+a' -Function BeginningOfLine -ViMode Insert
+# Set-PSReadLineKeyHandler -Chord 'Ctrl+e' -Function MoveToEndOfLine -ViMode Insert
+# Set-PSReadLineKeyHandler -Chord 'Ctrl+a' -Function BeginningOfLine -ViMode Command
+# Set-PSReadLineKeyHandler -Chord 'Ctrl+e' -Function MoveToEndOfLine -ViMode Command
+Set-PSReadLineKeyHandler -Chord 'Ctrl+f' -Function ForwardChar
+Set-PSReadLineKeyHandler -Chord 'Ctrl+b' -Function BackwardChar
+Set-PSReadLineKeyHandler -Chord 'Alt+f' -Function ForwardWord
+Set-PSReadLineKeyHandler -Chord 'Alt+b' -Function BackwardWord
+Set-PSReadLineKeyHandler -Chord 'Alt+n' -Function NextHistory
+Set-PSReadLineKeyHandler -Chord 'Alt+p' -Function PreviousHistory
+Set-PSReadLineKeyHandler -Chord 'Alt+F' -Function SelectForwardWord
+Set-PSReadLineKeyHandler -Chord 'Alt+B' -Function SelectBackwardWord
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Set up global key bindings (e.g., Ctrl+T for files, Ctrl+R for history)
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Setup tab completion
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+Set-PsFzfOption -TabExpansion
+
